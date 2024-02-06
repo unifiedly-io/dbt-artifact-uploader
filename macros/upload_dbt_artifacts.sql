974451f713d0a2010a9f15c3749f42a178c83a82
Brad <merlosus@yahoo.com>
2024-02-06 19:25:57 +1000

    changed function calls

--- macros/upload_dbt_artifacts.sql
+++ macros/upload_dbt_artifacts.sql
@@ -1,89 +1,71 @@
-{% macro upload_dbt_artifacts(instant_push=True, filenames=[]) %}
-
-{% set src_dbt_artifacts = source('dbt_artifacts', 'artifacts') %}
-
-{{ create_artifact_resources() }}
-{% do log("Beginning metadata push to Snowflake", info=True) %}
-{% set remove_query %}
-    remove @{{ src_dbt_artifacts }} pattern='.*.json.gz';
-{% endset %}
-
-{% do log("Clearing existing files from Stage: " ~ remove_query, info=False) %}
-{% do run_query(remove_query) %}
-
-{# Put all json files into the stage: Returns a tuple of all files that got uploaded #}
-{% set put_query %}
-    put file://target/*.json @{{ src_dbt_artifacts }} auto_compress=true;
-{% endset %}
-
-{% do log("Uploading files to Stage: " ~ put_query, info=False) %}
-{% set results = run_query(put_query) %}
-{% do log("Staged artifacts: " ~ results.columns[0].values(), info=False) %}
-
-{% set staged_artifacts = results.columns[0].values() %}
-
-{# Filter the objects that went into the stage to only the default dbt metadata #}
-{% if filenames %}
-    {% set valid_artifacts = filenames  %}
-{% else %}
-    {% set valid_artifacts = ['manifest.json', 'run_results.json', 'catalog.json']  %}
-{% endif %}
-
-{% set filtered_artifacts = []  %}
-
-{% for val in staged_artifacts %}
-  {% if val in valid_artifacts %}
-    {{ filtered_artifacts.append(val) }}
-  {% endif %}
-{% endfor %}
-
-{% do log("Filtered artifacts: " ~ filtered_artifacts, info=False) %})
-
-{% if filtered_artifacts | length > 0 %}
-    {# Push valid metadata objects into the metadata table #}
-    {% for filename in filtered_artifacts %}
-
-        {% set file = filename %}
-        
-        print(filename)
-
-
-        {% set copy_query %}
-            begin;
-            copy into {{ src_dbt_artifacts }} from
-                (
-                    select
-                    '{{ project_name }}' as project_name,
-                    $1 as data,
-                    $1:metadata:generated_at::timestamp_ntz as generated_at,
-                    metadata$filename as path,
-                    regexp_substr(metadata$filename, '([a-z_]+.json)') as artifact_type
-                    from  @{{ src_dbt_artifacts }}
-                )
-                file_format=(type='JSON')
-                on_error='skip_file';
-            commit;
-        {% endset %}
-
-        {% do log("Copying " ~ file ~ " from Stage: " ~ copy_query, info=False) %}
-        {% do run_query(copy_query) %}
-
-    {% endfor %}
-    {% do log("Metadata pushed to Snowflake", info=True) %})
-    {% if instant_push %}
-        {% do log("Pushing via Unifiedly", info=True) %}
-        {% set results = run_query(selectstar_ingestion_dbt()) %}
-        {% do log("Unifiedly Response:", info=True) %}
-        {% do print(results.columns.values()) %}
-    {% endif %}
-
-{% else %}
-
-    {% do log("No artifacts were found with valid names. Check your filename arguments", info=True) %})
-
-{% endif %}
-
-{% do log("Clearing new files from Stage: " ~ remove_query, info=False) %}
-{% do run_query(remove_query) %}
-
+{% macro selectstar_ingestion_dbt() %}
+-- Gather the dbt artifacts which have been uploaded for each project
+with artifacts as (
+    SELECT
+        p.project_name,
+        p.catalog::variant as catalog,
+        p.manifest::variant as manifest,
+        p.run_results::variant as run_results
+    from (
+        -- Get the latest of each artifact type for each project
+        select project_name, data, artifact_type from {{ source('dbt_artifacts', 'artifacts') }}
+        qualify row_number() over (partition by project_name, artifact_type order by generated_at desc) = 1
+    )
+    -- We need them to be all in a single row so that they are accessable when calling the API
+    pivot(max(data) for artifact_type in ('catalog.json', 'manifest.json', 'run_results.json'))
+        as p (project_name, catalog, manifest, run_results)
+)
+-- We need to send the data with GUID to identify each data source
+-- So we create a table with the name and GUID as currently found in Select Star
+-- by calling the data-sources api
+, data_sources as (
+    select
+        res.value:name::varchar as name
+        , res.value:guid::varchar as guid
+    from (
+        select 
+            api_data:results as results
+        from (
+        select unifiedly_app_instance
+            .public.selectstar_get('data-sources', {}) as api_data
+            )
+    ), lateral flatten(input=> results) as res
+)
+-- Joining the GUID into the artifacts data gives us everything we need to call
+-- the ingestion API
+, prepped_data as (
+    select
+        name
+        , guid
+        , manifest
+        , catalog
+        , run_results
+    from data_sources
+    join artifacts on data_sources.name = artifacts.project_name
+)
+-- We call the ingestion API for dbt and provide the artifacts as JSON objects
+-- The API returns the data source GUID if it was successful so we add a check
+-- so that we can validate the response by parsing the return and comparing to
+-- the guid column
+select
+    current_timestamp() as processed_datetime
+    , name
+    , guid
+    , unifiedly_app_instance.public.selectstar_post(
+        'ingestion/dbt',
+        {
+            'data_source': guid
+        },
+        {},
+        {
+            'manifest_file': to_json(manifest),
+            'catalog_file': to_json(catalog),
+            'run_results_file': to_json(run_results)
+        }
+    ) as api_response
+    , case
+        when api_response:data_source = guid then TRUE
+        else FALSE
+    end as update_successful
+    from prepped_data
 {% endmacro %}
