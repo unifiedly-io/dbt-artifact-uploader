9501da208227773ff1aa65608ee21ade70660ed5
Brad <merlosus@yahoo.com>
2024-03-22 18:41:08 +1000

    Updated docs and made it so select star could be used independently

--- macros/selectstar_ingestion_dbt.sql
+++ macros/selectstar_ingestion_dbt.sql
@@ -2,7 +2,7 @@
 
 {% do log("Pushing to Select Star via Unifiedly", info=True) %}
 {% set results = run_query(_selectstar_ingestion_dbt()) %}
-{% do log("Unifiedly Response: " + results.columns[0].values(), info=True) %}
-{% do print(results.columns.values()) %}
+{% do log("Unifiedly Response: "  info=True) %}
+{% do print(results.columns[0].values(),) %}
 
 {% endmacro %}
